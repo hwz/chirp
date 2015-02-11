069f406fbf54a3b9d341c90d79cc219090fdacef
Helen Zeng <helen.w.zeng@gmail.com>
2015-02-11 02:38:13 -0800

    separate sign up and log in

--- chirp/public/javascripts/chirpApp.js
+++ chirp/public/javascripts/chirpApp.js
@@ -1,6 +1,7 @@
 var app = angular.module('chirpApp', ['ngRoute', 'ngResource']).run(function($http, $rootScope) {
 	$rootScope.authenticated = false;
 	$rootScope.current_user = 'Guest';
+
 	$rootScope.signout = function(){
 		$http.get('auth/signout');
 		$rootScope.authenticated = false;
@@ -16,8 +17,13 @@ app.config(function($routeProvider){
 			controller: 'mainController'
 		})
 		//the login display
-		.when('/auth', {
-			templateUrl: 'auth.html',
+		.when('/login', {
+			templateUrl: 'login.html',
+			controller: 'authController'
+		})
+		//the signup display
+		.when('/signup', {
+			templateUrl: 'signup.html',
 			controller: 'authController'
 		})
 });
@@ -36,10 +42,10 @@ app.factory('postService', function($http){
 app.factory('postService', function($resource){
 	return $resource('/api/posts/:id');
 });
+
 app.controller('mainController', function($scope, $http, $rootScope, postService){
 	$scope.posts = postService.query();
 	$scope.newPost = {created_by: $rootScope.current_user, text: '', created_at: 0};
-
 /*
 //used for basic read from json
 	postService.getAll().success(function(data){
@@ -57,13 +63,10 @@ app.controller('mainController', function($scope, $http, $rootScope, postService
 		$scope.posts = postService.query();
 	};
 });
-
-
 app.controller('authController', function($scope, $http, $rootScope, $location){
 	$scope.user = {username: '', password: ''};
 	$scope.newUser = {username: '', password: ''};
-	$scope.reg_error_message = '';
-	$scope.login_error_message = '';
+	$scope.error_message = '';
 
 	$scope.login = function(){
 		$http.post('/auth/login', $scope.user).success(function(data){
@@ -73,7 +76,7 @@ app.controller('authController', function($scope, $http, $rootScope, $location){
 				$location.path('/');
 			}
 			else{
-				$scope.login_error_message = data.message;
+				$scope.error_message = data.message;
 			}
 		});
 	};
@@ -86,7 +89,7 @@ app.controller('authController', function($scope, $http, $rootScope, $location){
 				$location.path('/');
 			}
 			else{
-				$scope.reg_error_message = data.message;
+				$scope.error_message = data.message;
 			}
 		});
 	};
