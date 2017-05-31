541a916bf1fcb61bd543ab488384f825f6ef4267
Kapil Sanchihar <kapilsanchihar@gmail.com>
2017-05-31 14:20:14 -0500

    Revert "added delete functionality"
    This reverts commit 554cdfa888ee38aae8cdf51aea5c8c4313d8347d.

--- chirp/models/models.js
+++ chirp/models/models.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var postSchema = new mongoose.Schema({
 	created_by: String,		//should be changed to ObjectId, ref "User"
 	created_at: {type: Date, default: Date.now},
-	text: String	
+	text: String
 });
 
 var userSchema = new mongoose.Schema({
